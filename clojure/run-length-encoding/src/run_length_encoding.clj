3a5207192f6f5035cf1ec18d5c3a0551bbf9a9a0
Sankara Rameswaran <hello@sankara.me>
2017-12-14 16:58:08 -0800

    Optimizing the code

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -3,17 +3,11 @@
 (defn run-length-encode
   "encodes a string with run-length-encoding"
   [s]
-  ;; If current same as previous, inc count, else append, reset counter.
-  (loop [current-str (seq s)
-         count-tuples []]
-  (if (empty? current-str)
-    (apply str (flatten count-tuples))
-    (let [[count prev] (last count-tuples)
-          current-char (first current-str)]
-      (recur (rest current-str)
-             (if (= prev current-char)
-               (conj (pop count-tuples) [(+ (or count 1) 1) current-char])
-               (conj count-tuples [nil current-char])))))))
+  (->> s
+       (partition-by identity)
+       (mapcat (juxt count first))
+       (remove #{1})      
+       (clojure.string/join)))
 
 ;; (run-length-encode "WWBBBADASD")
 ;; (run-length-encode "AA")
@@ -23,15 +17,10 @@
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [s]
-  (loop [encoded-seq (re-seq #"(\d+)?(\D)" s)
-         decoded-str ""]
-    (if (empty? encoded-seq)
-      decoded-str
-      (let [[_ c-str character] (first encoded-seq)
-            count (if (nil? c-str) 1 (Integer/parseInt c-str))]
-        (recur
-         (rest encoded-seq)
-         (apply str decoded-str (repeat count character)))))))
+  (->> s
+       (re-seq #"(\d+)?(\D)")
+       (mapcat (fn [[_ c-str character]] (repeat (Integer/parseInt (or c-str "1")) character)))
+       (clojure.string/join)))
 
 ;; (run-length-decode "2ABC3G")
 ;; (run-length-decode "ABC")
